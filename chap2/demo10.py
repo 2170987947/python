671b989c95ef214d4fb810c6f4b7abd8cdfbfcff
2170987947 <2170987947@qq.com>
2022-04-16 00:17:36 +0800

    练习

---
+++ chap2/demo10.py
@@ -0,0 +1,13 @@
+print(1 + 2)
+print(1 - 2)
+print(2 * 4)
+print(1 / 2)
+print(11 // 2)
+print(11 / 2)
+print(11 % 2)
+print(2 ** 3)
+# 一正一负整除向下取整
+print(-9 // 4)
+# 余数 = 被除数 - 除数 * 商
+print(9 % -4)
+print(-9 % 4)
---
+++ chap2/demo11.py
@@ -0,0 +1,4 @@
+a = 3 + 4
+print(a)
+a = b = c = 20
+print(a, b, c)
