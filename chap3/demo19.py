a53eea98871121863ddb88a26ea42f99a91a7921
2170987947 <2170987947@qq.com>
2022-04-17 00:12:03 +0800

    获取列表中的多个元素

---
+++ chap3/demo19.py
@@ -0,0 +1,11 @@
+# 姓名：fmm
+# 开发时间：2022/4/16 23:30
+lst = [10, 20, 30, 40, 50, 60, 70, 80]
+print(id(lst))
+print(lst[1:6:1])
+print(id(lst))
+print(lst[1:6:])
+print(lst[:6:])
+print(lst[::])
+print(lst[5:2:-2])
+print(lst[-2:-5:-2])
