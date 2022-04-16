14b03883055c3655f856086fbc50d7134e33b926
2170987947 <2170987947@qq.com>
2022-04-17 00:10:47 +0800

    创建列表对象

---
+++ chap3/demo17.py
@@ -0,0 +1,12 @@
+# 姓名：fmm
+# 开发时间：2022/4/16 23:13
+a = 10
+lst = ['hello', 'world', 98]
+print(id(lst))
+print(type(lst))
+print(lst)
+
+lst2 = list(['hello', 98, 'world'])
+print(id(lst2))
+print(type(lst2))
+print(lst2)
