396d95d7193c7972a4f1cf928fe8fb5bd2d99083
2170987947 <2170987947@qq.com>
2022-04-17 00:11:21 +0800

    获取列表中指定元素的索引

---
+++ chap3/demo18.py
@@ -0,0 +1,8 @@
+# 姓名：fmm
+# 开发时间：2022/4/16 23:21
+lst = ['hello', 'world', 98, 'hello']
+print(lst.index('hello'))
+print(lst.index('hello', 1, 4))
+# print(lst.index('99'))
+print(lst[-1])
+print(lst[2])
