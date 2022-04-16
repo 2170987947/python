7d8b75ebb349615f3b62f1ae62eceb44df871c4d
2170987947 <2170987947@qq.com>
2022-04-16 16:11:02 +0800

    对象的布尔值bool()

---
+++ chap3/demo.py
@@ -0,0 +1,19 @@
+# 姓名：fmm
+# 开发时间：2022/4/16 15:54
+print(bool(False))
+print(bool(0))
+print(bool(0.0))
+print(bool(None))
+# 空字符串
+print(bool(''))
+# 空列表
+print(bool([]))
+print(bool(list()))
+# 空元组
+print(bool(()))
+print(bool(tuple()))
+# 空字典
+print(bool({}))
+print(bool(dict()))
+# 空集合
+print(bool(set()))
