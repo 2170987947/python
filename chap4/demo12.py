1233aa4627fa330f27bfc6bdd5ad0a800687c806
2170987947 <2170987947@qq.com>
2022-04-17 16:39:00 +0800

    集合的创建

---
+++ chap4/demo12.py
@@ -0,0 +1,18 @@
+# 姓名：fmm
+# 开发时间：2022/4/17 16:29
+s = {1, 2, 3, 4, 3, 2, 9}
+print(s)
+s2 = set(range(6))
+print(s2)
+s3 = set([1, 2, 3, 2, 0, 8])
+print(s3)
+s4 = set((1, 2, 3))
+print(s4)
+s5 = set('Python')
+print(s5)
+s6 = set({1, 2, 3, 4})
+print(s6)
+s7 = set()
+print(s7, type(s7))
+s8 = dict()
+print(s8, type(s8))
