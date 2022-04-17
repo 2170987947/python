81aa194486f024e9ddfefeb8a89052fed1fb7594
2170987947 <2170987947@qq.com>
2022-04-17 16:52:20 +0800

    集合的相关操作

---
+++ chap4/demo13.py
@@ -0,0 +1,15 @@
+# 姓名：fmm
+# 开发时间：2022/4/17 16:39
+s = {10, 20, 30, 40, 50}
+print(s)
+s.add(60)
+print(s)
+s.update({200, 300, 400})
+print(s)
+s.update([100, 99, 98])
+print(s)
+s.update((101, 102, 103))
+print(s)
+s.update('Python')
+print(s)
+s.remove(10)
