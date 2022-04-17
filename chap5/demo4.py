3512fa972efee54ecf89281c0859d6f67a2a51a4
2170987947 <2170987947@qq.com>
2022-04-18 01:08:18 +0800

    字符串内容对齐操作的方法

---
+++ chap5/demo4.py
@@ -0,0 +1,12 @@
+# 姓名：fmm
+# 开发时间：2022/4/18 0:50
+s = 'hello, Python'
+print(s.center(20, '*'))
+print(s.ljust(20, '*'))
+print(s.ljust(10, '*'))
+print(s.ljust(10))
+print(s.rjust(20, '*'))
+print(s.zfill(20))
+print('+8970'.zfill(8))
+print('-8970'.zfill(8))
+print('=8970'.zfill(8))
