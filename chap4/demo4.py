85b671a312c932d1eff06c1e8b06d37e2587c743
2170987947 <2170987947@qq.com>
2022-04-17 13:22:06 +0800

    获取字典视图

--- chap4/demo4.py
+++ chap4/demo4.py
@@ -1,6 +1,6 @@
 # 姓名：fmm
 # 开发时间：2022/4/17 13:10
-scores = {'张三': 89, '李四': 59, '王五': 20}
+scores = {'张三': 89, '李四': 59, '王五': 20, '小六': 99}
 print(scores.keys())
 print(scores.values())
 print(scores.items())
