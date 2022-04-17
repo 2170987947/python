bce9d3c77d6dbc1a82e73da57e1f599c634f5398
2170987947 <2170987947@qq.com>
2022-04-17 14:58:26 +0800

    元组中的元素可以使可变对象也可以是不可变对象

---
+++ chap4/demo10.py
@@ -0,0 +1,9 @@
+# 姓名：fmm
+# 开发时间：2022/4/17 14:51
+t = (10, [20, 30], 40)
+print(t, type(t))
+print(t[0], type(t[0]))
+print(t[1], type(t[1]))
+print(t[2], type(t[2]))
+t[1][0] = 30
+print(t, type(t))
