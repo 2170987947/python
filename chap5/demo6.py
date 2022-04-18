6f744a8afe5e0fe7b66bf829f0b4b7f8e7283c79
2170987947 <2170987947@qq.com>
2022-04-18 15:34:51 +0800

    判断字符串操作的方法

---
+++ chap5/demo6.py
@@ -0,0 +1,9 @@
+# 姓名：fmm
+# 开发时间：2022/4/18 12:38
+s = 'hello, python'
+print(s.isidentifier())
+print(s.isspace())
+print(s.isalpha())
+print(s.isdecimal())
+print(s.isnumeric())
+print(s.isalnum())
