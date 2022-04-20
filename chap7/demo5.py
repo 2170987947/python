32255d7a9230dec0665daa74b1b7a7c5d58edde1
2170987947 <2170987947@qq.com>
2022-04-20 10:47:58 +0800

    模块的导入

---
+++ chap7/demo5.py
@@ -0,0 +1,11 @@
+# 姓名：fmm
+# 开发时间：2022/4/20 10:10
+import math
+print(id(math))
+print(type(math))
+print(math)
+print(math.pi)
+print(dir(math))
+print(math.pow(2, 3))
+print(math.ceil(2.001))
+print(math.floor(2.001))
