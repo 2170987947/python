a96f1da1e62cca0927008474558916ffeb0071de
2170987947 <2170987947@qq.com>
2022-04-20 11:08:39 +0800

    以主程序形式运行

---
+++ chap7/calc2.py
@@ -0,0 +1,6 @@
+# 姓名：fmm
+# 开发时间：2022/4/20 10:56
+def add(a, b):
+    return a + b
+if __name__ == '__main__':
+    print(add(10, 20))
---
+++ chap7/dmeo8.py
@@ -0,0 +1,4 @@
+# 姓名：fmm
+# 开发时间：2022/4/20 10:58
+import calc2
+print(calc2.add(100, 200))
