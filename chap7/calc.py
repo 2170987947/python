52ec29481c419a08ff7ccfa9e81ee5e77fdd86a1
2170987947 <2170987947@qq.com>
2022-04-20 10:53:09 +0800

    自定义模块的导入

---
+++ chap7/calc.py
@@ -0,0 +1,6 @@
+# 姓名：fmm
+# 开发时间：2022/4/20 10:50
+def add(a, b):
+    return a + b
+def div(a, b):
+    return a / b
---
+++ chap7/demo7.py
@@ -0,0 +1,4 @@
+# 姓名：fmm
+# 开发时间：2022/4/20 10:51
+import calc
+print(calc.add(2, 9))
