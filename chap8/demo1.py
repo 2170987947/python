6206b3511559d6be7207ee429763f9453613ec39
2170987947 <2170987947@qq.com>
2022-04-21 23:46:56 +0800

    IO操作

--- chap8/demo1.py
+++ chap8/demo1.py
@@ -1,5 +1,7 @@
 # 姓名：fmm
 # 开发时间：2022/4/20 11:50
 file = open('a.txt', 'r', encoding='UTF-8')
-print(file.readlines())
+target_file = open('c.txt', 'w', encoding='utf-8')
+target_file.write(file.read())
 file.close()
+target_file.close()
--- chap8/demo3.py
+++ chap8/demo3.py
@@ -1,5 +1,7 @@
 # 姓名：fmm
 # 开发时间：2022/4/21 23:33
-file = open('CET-4.jpg', 'rb', encoding="utf-8")
-target_file = open('CET-41.jpg', 'wb', encoding="utf-8")
-target_file.write(file)
+file = open('CET-4.jpg', 'rb')
+target_file = open('CET-41.jpg', 'wb')
+target_file.write(file.read())
+file.close()
+target_file.close()
