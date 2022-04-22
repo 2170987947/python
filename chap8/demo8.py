a33270dea03287770cf34a5859b18d5b3490b0f9
2170987947 <2170987947@qq.com>
2022-04-22 13:34:00 +0800

    OS模块操作目录的函数

---
+++ chap8/demo8.py
@@ -0,0 +1,11 @@
+# 姓名：fmm
+# 开发时间：2022/4/22 13:16
+import os
+print(os.getcwd())
+print(os.listdir('D:\\fmm\\tenement-master'))
+# print(os.mkdir('ee'))
+# print(os.makedirs('dd/ff'))
+# print(os.rmdir('ee'))
+# print(os.removedirs('dd/ff'))
+print(os.chdir('D:\Git\python\python\chap7'))
+print(os.getcwd())
