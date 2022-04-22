16569cb7694208ec62787526380a1868a93519b1
2170987947 <2170987947@qq.com>
2022-04-22 13:15:25 +0800

    OS的常用函数

---
+++ chap8/demo7.py
@@ -0,0 +1,7 @@
+# 姓名：fmm
+# 开发时间：2022/4/22 13:09
+import os
+# os.system('notepad.exe')
+# os.system('calc.exe')
+os.startfile('D:\\Program Files (x86)\\Tencent\WeChat\\WeChat.exe')
+os.startfile('D:\Program Files (x86)\Tencent\WeChat\WeChat.exe')
