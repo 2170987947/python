4b4127d9b1b906250a29ea894910bec646600ad7
2170987947 <2170987947@qq.com>
2022-04-24 09:01:54 +0800

    输出指定目录下的所有文件（包含子目录）

--- chap8/demo11.py
+++ chap8/demo11.py
@@ -2,6 +2,10 @@
 # 开发时间：2022/4/22 14:01
 import os
 for dirpath, dirname, filename in os.walk(os.getcwd()):
-    print(dirpath)
-    print(dirname)
-    print(filename)
+    # print(dirpath)
+    # print(dirname)
+    # print(filename)
+    for dir in dirname:
+        print(os.path.join(dirpath, dir))
+    for file in filename:
+        print(os.path.join(dirpath, file))
